2118d4bfdbd72fa3cb7178cba6e888316a0e1a21
Julia Barbosa <juliadealencarb@gmail.com>
2023-04-04 21:07:59 +0100

    adding accordeon effect.

--- src/components/Album/index.jsx
+++ src/components/Album/index.jsx
@@ -1,21 +1,38 @@
+import Accordion from 'react-bootstrap/Accordion';
+
 export default function Album({ handleLike, albums }) {
     return (
         <>
             {albums.map(album => (
                 <>
-                <img src={album.cover} alt={album.name} />
-                <h4>{album.name}</h4>
-                <span>{album.year}</span>
-                <ul>
-                    {album.songs.map(song => (
-                    <li onClick={() => handleLike(album.id, song)}> 
-                        {song.track} - {song.title} {song.liked ? <b>❤️</b> : <b> </b>}
-                    </li>
-                    ))}
-                </ul>
+                    <img src={album.cover} alt={album.name} />
+                    <h4>{album.name}</h4>
+                    <span>{album.year}</span>
+                    <Accordion>
+                        {album.songs.map(song => (
+                            <Accordion.Item eventKey={song.track + "." + album.id}>
+                                <Accordion.Header>{song.liked ? "❤️" : " "}{song.track} - {song.title}</Accordion.Header>
+                                <Accordion.Body>
+                                    Some text.
+                                    <button onClick={() => handleLike(album.id, song)}>Like</button>
+                                </Accordion.Body>
+                            </Accordion.Item>
+                        ))}
+                    </Accordion>
                 </>
             ))}
         <br />
         </>
     )
 }
+
+// 
+
+{/* <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>{song.liked ? "❤️" : " "}{song.track} - {song.title}</Accordion.Header>
+        <Accordion.Body>
+          Some text.
+        </Accordion.Body>
+      </Accordion.Item>
+    <button onClick={() => handleLike(album.id, song)}>Like</button> */}
