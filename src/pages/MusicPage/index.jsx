27f7c2f6a186f7ceb132edfdb87f0676f59e449e
Julia Barbosa <juliadealencarb@gmail.com>
2023-04-06 11:20:27 +0100

    Adding music page.

--- src/pages/MusicPage/index.jsx
+++ src/pages/MusicPage/index.jsx
@@ -1,7 +1,38 @@
+import React, { useState, useEffect, useRef } from "react";
+import { useAlbums } from "../../contexts";
+import Accordion from 'react-bootstrap/Accordion';
+
+
 export default function Music() {
+
+    const { albums, setLikedMusic } = useAlbums();
+
+    async function getLyrics(songTitle) {
+
+        const lyrics = await fetch(`https://api.lyrics.ovh/v1/ghost/${songTitle}`)
+    }
+
     return (
-        <div>
-            <h1>Ghost 👻 songs</h1>
-        </div>
+        <>
+            {albums.map(album => (
+                <>
+                    <img src={album.cover} alt={album.name} />
+                    <h4>{album.name}</h4>
+                    <span>{album.year}</span>
+                    <Accordion>
+                        {album.songs.map(song => (
+                            <Accordion.Item eventKey={song.track + "." + album.id}>
+                                <Accordion.Header>{song.liked ? "❤️" : " "}{song.track} - {song.title}</Accordion.Header>
+                                <Accordion.Body>
+                                    Some text.
+                                    <button onClick={() => handleLike(album.id, song)}>Like</button>
+                                </Accordion.Body>
+                            </Accordion.Item>
+                        ))}
+                    </Accordion>
+                </>
+            ))}
+        <br />
+        </>
     )
 }
