92829e3e80ca8b1fa1b0576bd4132bd5aa021b7b
Julia Barbosa <juliadealencarb@gmail.com>
2023-04-07 18:56:16 +0100

    adding lyrics functionality.

--- src/pages/MusicPage/index.jsx
+++ src/pages/MusicPage/index.jsx
@@ -4,17 +4,15 @@ import Accordion from 'react-bootstrap/Accordion';
 
 
 export default function Music() {
-
+    const [lyrics, setLyrics] = useState("");
     const { albums, setLikedMusic } = useAlbums();
 
-    useEffect(() => {
-        async function getLyrics(songTitle) {
+    async function getLyrics(songTitle) {
+        const res = await fetch(`https://api.vagalume.com.br/search.php?art=ghost&mus=${songTitle}&apikey=e19e41474ed897f08530acbc527ef419`)
+        const lyrics = await res.json();
 
-            const res = await fetch(`https://api.vagalume.com.br/search.php?art=ghost&mus=${songTitle}&apikey=e19e41474ed897f08530acbc527ef419`)
-            const lyrics = await res.json();
-            console.log(lyrics.mus[0].text)
-        }
-    }, [])
+        setLyrics(lyrics.mus[0].text)
+    }
 
     function handleLike(albumId, song) {
         const mutatedAlbums = albums.map((album, index) => {
@@ -38,10 +36,15 @@ export default function Music() {
                     <span>{album.year}</span>
                     <Accordion>
                         {album.songs.map(song => (
-                            <Accordion.Item eventKey={song.track + "." + album.id}>
+                            <Accordion.Item eventKey={song.track + "." + album.id} onClick={() => getLyrics(song.title)}>
                                 <Accordion.Header>{song.liked ? "❤️" : " "}{song.track} - {song.title}</Accordion.Header>
                                 <Accordion.Body>
-                                    Lyrics will be here.
+                                    { lyrics.split('\n').map(line => (
+                                        <div>
+                                            {line}
+                                            <br></br>
+                                        </div>
+                                    ))}
                                     <button onClick={() => handleLike(album.id, song)}>Like</button>
                                 </Accordion.Body>
                             </Accordion.Item>
